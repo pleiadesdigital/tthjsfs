652b2e4d4d0272cf7ed8c9a49ce2a908cfe62719
Ronaldo Ortiz <ronyortizop@yahoo.com>
2019-05-10 09:21:14 -0400

    VID 5.3.3 DONE

--- chap5/js/app5-3.js
+++ chap5/js/app5-3.js
@@ -11,11 +11,34 @@ const button = document.querySelector("button");
   p.textContent = input.value + ':';
 }); */
 
+/* VIDEO 5.3.3 */
+let inputType = input.type;
+console.log(inputType);
+const inputClass = input.className;
+console.log(inputClass);
+
 button.addEventListener('click', () => {
+  p.textContent = input.value + ':';
+});
+
+p.title = "List description";
+
+// changin the type attribute to 'input'
+/* input.type = 'checkbox';
+inputType = input.type;
+console.log(inputType); */
+
+
+
+
+
+/* VIDEO 5.2.2 */
+
+/* button.addEventListener('click', () => {
   p.innerHTML = input.value + ':';
 });
 
 let ul = document.querySelector("ul");
 console.log(ul.innerHTML);
 
-ul.innerHTML = "<li>blue cabbage</li>";
+ul.innerHTML = "<li>blue cabbage</li>"; */
